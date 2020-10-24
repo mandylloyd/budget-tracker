e8c594dcc182ceebc8b661e20a2f1d0c6871d8f0
honey bee <honeybee@users.noreply.github.com>
2020-10-24 01:54:32 -0400

    deploying

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+// files to cache
+
+// install -- uses waitUntil() and skipWaiting()
+self.addEventListener("install", function(evt) {
+
+})
+
+// activate -- uses waitUntil()
+self.addEventListener("activate", function() {
+
+})
+
+// fetch -- uses startsWith
+self.addEventListener("fetch", function() {
+
+})
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
